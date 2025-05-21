f5aaa121d36644ff3c24de2defcbf0935bbaaa7c
NANI <velpulanani96@gmail.com>
2025-05-21 15:13:12 +0530

    Fix Python environment and package installation

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/bin/zsh
 
+# Activate the virtual environment
+source venv/bin/activate
+
+# Run the Flask application
 python3 -m flask run
--- setup.sh
+++ setup.sh
@@ -2,4 +2,13 @@
 
 export FLASK_APP=app
 export FLASK_DEBUG=1
+
+# Create and activate a virtual environment
+python3 -m venv venv
+source venv/bin/activate
+
+# Install the required packages
+pip install --upgrade pip
+pip install -r requirements.txt
+
 source ./keys.sh # set env DATABASE_URL
